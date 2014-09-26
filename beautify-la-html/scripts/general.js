3828b3173270b6f435336fee72f84c8180d56927
Thao Tran <thaotran@vinasource.com>
2014-09-26 15:13:02 +0700

    fix bug for forgot and term popup

--- beautify-la-html/scripts/general.js
+++ beautify-la-html/scripts/general.js
@@ -1,9 +1,4 @@
-$(window).resize(function(){location.reload();});
-
-$(document).ready(function(){	
-	
-	//begin selectmenu (customize style for select)
-	$('#select-city').selectmenu();
+$(document).ready(function(){		
 	
 	var isMobile = {
 		Android: function() {
@@ -26,76 +21,8 @@ $(document).ready(function(){
 		}
 	};
 			
-	if( !isMobile.any() ) {
-		
+	if( isMobile.any() ) {
+		$('detech-devices').addClass('detech-devices');
 	};
-	
-	var getSolution = $(window).width();	
-	if(getSolution > 980){
-		sticky();
-	}
-	
-	function sticky(){
-		//begin scroll
-		var footerHeight =  $('.site-footer').height();
-		$(window).scroll(function () {
-			var winTop = $(this).scrollTop(),
-				winBottom = winTop + $(this).height(),
-				heightHeader = $('.site-header').height() + $('#carousel-desktop').height(),
-				right = $('#nav-affix'),
-				rightBottom = right.height() + heightHeader,
-				left = $('#main-affix'),
-				leftBottom = left.height() + heightHeader;
-				heightContact = $('.site-contact').innerHeight();
-				offsetContact = $('.site-contact').offset().top;
-				console.log(heightContact);
-				
-			if (rightBottom < leftBottom) {
-				//when the user reached the bottom of '#nav-affix' set its position to fixed to prevent it from moving on scroll
-				if (winBottom >= rightBottom) {
-					if(winBottom <= offsetContact){
-						right.css({
-							'position': 'fixed',
-							'bottom': 0
-						});
-					}else{
-						right.css({
-							'position': 'fixed',
-							'bottom': footerHeight + heightContact
-						});
-					}
-				}else {
-					//when the user scrolls back up revert its position to relative
-					right.css({
-						'position': 'relative',
-						'bottom': 'auto'
-					});
-				}
-			} else {
-				//when the user reached the bottom of '#main-affix' set its position to fixed to prevent it from moving on scroll
-				if (winBottom >= leftBottom) {
-					if(winBottom <= offsetContact){
-						left.css({
-							'position': 'fixed',
-							'bottom': 0
-						});
-					}else{
-						left.css({
-							'position': 'fixed',
-							'bottom': footerHeight + heightContact
-						});
-					}
-				} else {
-					//when the user scrolls back up revert its position to relative
-					left.css({
-						'position': 'relative',
-						'bottom': 'auto'
-					});
-				}
-			}
-	
-		});
-		//end scroll	
-	}
 		
 });
