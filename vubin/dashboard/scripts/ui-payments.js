c3460e89931709ab479edca2ff70caa441d16596
Thao Tran <thaotran@vinasource.com>
2016-08-02 18:39:45 +0700

    update icon and payment

--- vubin/dashboard/scripts/ui-payments.js
+++ vubin/dashboard/scripts/ui-payments.js
@@ -5,20 +5,43 @@ $(document).ready(function () {
     $('.dl-list .radio-mark').each(function () {
         var isChecked = $(this).is(':checked');
         if (isChecked === true) {
-            $(this).closest('.dl').addClass('active');
+            $(this).closest('.dl-item-item').addClass('active');
         } else {
-            $(this).closest('.dl').removeClass('active');
+            $(this).closest('.dl-item').removeClass('active');
         }
     }).on('click', function () {
         var isChecked = $(this).is(':checked');
-        $('.dl-list').find('.dl').removeClass('active');
+        $('.dl-list').find('.dl-item').removeClass('active');
         if (isChecked === true) {
-            $(this).closest('.dl').addClass('active');
+            $(this).closest('.dl-item').addClass('active');
         } else {
-            $(this).closest('.dl').removeClass('active');
+            $(this).closest('.dl-item').removeClass('active');
         }
     });
 
     iscrollSelect('divTimePolicy', 'iscrollTimePolicy');
     iscrollSelect('divRefundPolicy', 'iscrollRefundPolicy');
+
+    mobilePolicy();
+});
+
+function mobilePolicy(){
+    if($(window).width() < 1024){
+        $('.dl-list .dl-item').on('click', function (e) {
+            var carouselControl = $(e.target).hasClass('carousel-control');
+            if(carouselControl){
+                return;
+            }
+            $('.dl-list').find('.dl-item').removeClass('active');
+            $('.dl-list').find('.radio-mark').prop('checked', 'false');
+            $(this).addClass('active');
+            $(this).find('.radio-mark').prop('checked', 'true');;
+        });
+    }
+}
+
+$(window).resize(function(){
+    fixHeight('.dl-list', '.time-policy');
+
+    mobilePolicy();
 });
