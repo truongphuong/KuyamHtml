77ca5a9d5a3f872fa0a028b50a8e27d34258ebe3
Thao Tran <thaotran@vinasource.com>
2016-11-24 14:29:00 +0700

    book details

--- Kuyam-home/public/content/js/pages/ui-book-details.js
+++ Kuyam-home/public/content/js/pages/ui-book-details.js
@@ -36,29 +36,104 @@ function loadmap(lat, lon, googleIcon) {
     });
 }
 $(document).ready(function () {
-    $('.select-options').click(function (e) {
-        $(".options-icon").removeClass("z-index-12");
-        $(this).find(".options-icon").addClass("z-index-12");
-    });
-
-    $('.options-text li.options-item').click(function (e) {
-        var a = $(e.target).attr('class');
-        if (a != "link-service" || a != "link-class") {
-            $(".bg-white-50").fadeOut();
-            $('.options-text li.options-item').removeClass("active");
-            $(this).addClass("active");
-            $('#select-who').fadeIn();
-            $('#select-who').find(".select-text").html("");
-            var getValue = $(this).find(".options-value").html();
-            $(this).parent().parent().parent().find(".select-text").html(getValue);
+    $(document).on('click', '.votes', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        var isFavorited = $this.find('.vote-icon').hasClass('icon-like');
+        if (isFavorited) {
+            $this.find('.vote-icon').removeClass('icon-like');
+        } else {
+            $this.find('.vote-icon').addClass('icon-like');
         }
     });
 
-    $('#staffSelect').selectpicker();
+    var categoryContent = '';
+    categoryContent += '<div id="categoryList">';
+    categoryContent += '<div class="content-scroll">';
+    categoryContent += '<ul>';
+    categoryContent += '<li>Lorem ipsum dolor</li>';
+    categoryContent += '<li>Service Lorem</li>';
+    categoryContent += '<li>Service ipsum</li>';
+    categoryContent += '<li>Lorem service</li>';
+    categoryContent += '<li>Lorem ipsum dolor</li>';
+    categoryContent += '<li>Lorem ipsum dolor</li>';
+    categoryContent += '<li>Service Lorem</li>';
+    categoryContent += '<li>Service ipsum</li>';
+    categoryContent += '<li>Lorem service</li>';
+    categoryContent += '<li>Lorem ipsum dolor</li>';
+    categoryContent += '</ul>';
+    categoryContent += '</div>';
+    categoryContent += '/div>';
+
+    $('.tooltipster-category').tooltipster({
+        contentAsHTML: true,
+        content: categoryContent,
+        trigger: 'custom',
+        position: 'right',
+        theme: 'tooltipster-default tooltipster-category',
+        offsetX: -5,
+        interactive: true,
+        positionTracker: true,
+        autoClose: false,
+        debug: false,
+        functionReady: function functionReady(origin, tooltip) {
+            iscrollContent('#categoryList');
+        }
+    });
+
+    $(document).on('click', '.option-select li', function (e) {
+        var $this = $(this);
+        var selectedVal = $this.find('.text').text();
+        $this.closest('.option-select').find('a .text').html(selectedVal);
+        $this.closest('.option-select').find('li').removeClass('selected');
+        $this.addClass('selected');
+
+        var isWhat = $this.closest('#whatSelect').hasClass('what-select');
+        if (isWhat) {
+            $('#whoSelect').removeClass('disabled');
+        } else {
+            $(document).find('.site-calendar .bg-white-50').addClass('hide');
+        }
+    });
 
     $('.session-item').hover(function () {
         $(this).addClass("active");
     }, function () {
         $(this).removeClass("active");
+    });
+
+    if (!isMobile.Windows()) {
+        iscrollContent('#servicesList');
+
+        $(window).resize(function () {
+            refreshNiceScroll('#servicesList');
+        });
+    }
+
+    $('#bookTabs a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var $this = $(e.target);
+        var listID = $this.attr('href') + 'List';
+        if (listID === '') {
+            return;
+        }
+        if (!isMobile.Windows()) {
+            iscrollContent(listID);
+
+            $(window).resize(function () {
+                refreshNiceScroll(listID);
+            });
+        }
+    });
+
+    $('#appointmentTabs a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var $this = $(e.target);
+        var listID = $this.attr('href') + 'List';
+        if (!isMobile.Windows()) {
+            iscrollContent(listID);
+
+            $(window).resize(function () {
+                refreshNiceScroll(listID);
+            });
+        }
     });
 });
