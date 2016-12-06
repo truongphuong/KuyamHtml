cbb2fe89f4b56de09452abb84c7308937d202854
Thao Tran <thaotran@vinasource.com>
2016-12-06 12:17:06 +0700

    about page

---
+++ vubin/dashboard/scripts/ui-about.js
@@ -0,0 +1,16 @@
+tinymce.init({
+    selector: '#txtAbout',
+    height: 500,
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste imagetools"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+    paste_data_images: true,
+    content_css: [
+        '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+        '//www.tinymce.com/css/codepen.min.css'
+    ]
+});
