05a94c63991ab1db6247f7356fa8ca2847ce8426
thaotran <thaotran@vinasource.com>
2017-03-15 11:23:21 +0700

    Event creation flow

--- beautify-la/public/content/js/pages/ui-company-event.js
+++ beautify-la/public/content/js/pages/ui-company-event.js
@@ -128,6 +128,27 @@ $(document).ready(function () {
 			$this.addClass('active');
 		}
 	});
+	// End week-list
 
-	// Begin week-list
+	// Begin owl
+	var owl = $("#serchPhotoOwl");
+
+	owl.owlCarousel({
+		pagination: false,
+		items: 3,
+		itemsDesktop: [1199, 3], //5 items between 1000px and 901px
+		itemsDesktopSmall: [991, 3], // betweem 900px and 601px
+		itemsTablet: [767, 2], //2 items between 600 and 0
+		itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+	});
+
+	// Custom Navigation Events
+	$("#searchSection .next").click(function () {
+		owl.trigger('owl.next');
+	});
+
+	$("#searchSection .prev").click(function () {
+		owl.trigger('owl.prev');
+	});
+	// End owl
 });
